322c13bdc153c13079d2c760e9f1cf12b520389d
Turbodiesel92 <sirmatthew@DESKTOP-E2O47QT.localdomain>
2022-03-03 12:21:33 -0600

    pinia course

---
+++ src/stores/ProductStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+export const useProductStore = defineStore("ProductStore", {
+    state: () => {
+        return {
+            products: []
+        };
+    },
+    actions: {
+        async fill() {
+            this.products = (await import("@/data/products.json")).default;
+            // this.products = (await axios.get('some/end/point')).data
+        },
+    },
+    // getters
+})
